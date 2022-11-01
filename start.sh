e8c61c42f342353d12ea6df424d87de8fb73b045
makstashkevich <makstashkevich@gmail.com>
2022-11-01 22:11:55 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,8 +5,11 @@ if ! [ -x "$(command -v docker)" ]; then
   sudo ./scripts/init_docker_engine.sh
 fi
 
+echo "Remove old bridge image ..."
+sudo docker rmi bridge
+echo
 echo "Build bridge image ..."
 sudo docker build -t bridge .
 echo
 echo "Run bridge ..."
-sudo docker run -p 3000:3000 bridge
+sudo docker run -p 3000:3000 --rm bridge
